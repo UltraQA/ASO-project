3d49b553c79d38b072f796699ef03c52c52d0644
Andrei Mironchik <andreimironchik@MacBookPro-Andrei.local>
2021-01-28 09:12:53 +0100

    added Duplicates function

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const subtitleTextCharactersAmount = document.getElementById('subTitleText');
 const keyWordsAmount = document.getElementById('keyWords');
 const keyWordsCharactersAmount = document.getElementById('keyWords');
 const allTextFromAllTextArea = document.getElementById('page');
+const searchDuplicates = document.getElementById('page');
 
 titleTextWordsAmount.addEventListener('input', () => {
   let text = document.getElementById('titleText').value;
@@ -24,17 +25,8 @@ titleTextCharactersAmount.addEventListener('input', () => {
 
   if (textlength > maxLength) {
     document.getElementById('titleCharacters').innerHTML =
-      '<span style="background-color: #f44336; color: white">' +
-      textlength +
-      ' out of ' +
-      maxLength +
-      ' </span>';
-    // document.getElementById("titleTextWords").innerHTML = '<h4 id="titleTextWords" style="color: red;">Title text</h4>';
-    console.log(
-      '%c%s',
-      'color: white; background: #f44336; font-size: 16px;',
-      `Title length is ${textlength}/30`
-    );
+      '<span style="background-color: #f44336; color: white">' + textlength + ' out of ' + maxLength + ' </span>';
+    console.log('%c%s', 'color: white; background: #f44336; font-size: 16px;', `Title length is ${textlength}/30`);
   } else {
     document.getElementById('titleCharacters').innerHTML = textlength + ' out of ' + maxLength;
   }
@@ -58,16 +50,8 @@ subtitleTextCharactersAmount.addEventListener('input', () => {
 
   if (textlength > maxLength) {
     document.getElementById('SubTitleCharacters').innerHTML =
-      '<span style="background-color: #f44336; color: white">' +
-      textlength +
-      ' out of ' +
-      maxLength +
-      '</span>';
-    console.log(
-      '%c%s',
-      'color: white; background: #f44336; font-size: 16px;',
-      `Sub-title length is ${textlength}/30`
-    );
+      '<span style="background-color: #f44336; color: white">' + textlength + ' out of ' + maxLength + '</span>';
+    console.log('%c%s', 'color: white; background: #f44336; font-size: 16px;', `Sub-title length is ${textlength}/30`);
   } else {
     document.getElementById('SubTitleCharacters').innerHTML = textlength + ' out of ' + maxLength;
   }
@@ -91,16 +75,8 @@ keyWordsCharactersAmount.addEventListener('input', () => {
 
   if (textlength > maxLength) {
     document.getElementById('keyWordcharacters').innerHTML =
-      '<span style="background-color: #f44336; color: white;">' +
-      textlength +
-      ' out of ' +
-      maxLength +
-      '</span>';
-    console.log(
-      '%c%s',
-      'color: white; background: #f44336; font-size: 16px;',
-      `keyWords length is ${textlength}/30`
-    );
+      '<span style="background-color: #f44336; color: white;">' + textlength + ' out of ' + maxLength + '</span>';
+    console.log('%c%s', 'color: white; background: #f44336; font-size: 16px;', `keyWords length is ${textlength}/30`);
   } else {
     document.getElementById('keyWordcharacters').innerHTML = textlength + ' out of ' + maxLength;
   }
@@ -111,16 +87,18 @@ allTextFromAllTextArea.addEventListener('input', () => {
   let subTitleTextArea = document.getElementById('subTitleText').value;
   let keyWordsTextArea = document.getElementById('keyWords').value;
   let allTextArea = titleTextArea + ' ' + subTitleTextArea + ' ' + keyWordsTextArea;
+  let allTextAreaValue = titleTextWordsAmount.value + ' ' + subtitleTextWordsAmount.value + ' ' + keyWordsAmount.value;
+  let regEx = /\s|\,/;
+  //replace all spaces from income text to ','
+  let newString = allTextArea.replace(/\s|\,/gi, ',');
+  //trying to find duplicates items
+  let arr = newString.toLowerCase().split(',');
+  let uniqueItems = 0;
+  uniqueItems = arr.filter((item, index) => {
+    return arr.indexOf(item) !== index;
+  });
   //If all text areas are filled - then we join all texts split them and store in finalString array
   if (titleTextArea.length > 0 && subTitleTextArea.length > 0 && keyWordsTextArea.length > 0) {
-    let uniqueItems = 0;
-    let allStrings =
-      titleTextWordsAmount.value + ' ' + subtitleTextWordsAmount.value + ' ' + keyWordsAmount.value;
-    let regEx = /\s|\,/;
-    //replace all spaces from income text to ','
-    let newString = allTextArea.replace(/\s|\,/gi, ',');
-    let arr = newString.toLowerCase().split(',');
-
     function splitString(stringToSplit, separator) {
       let arrayOfStrings = stringToSplit.split(separator);
       console.log(
@@ -129,13 +107,30 @@ allTextFromAllTextArea.addEventListener('input', () => {
         'Array contains ' + arrayOfStrings.length + ' words: ' + arrayOfStrings.join(' / ')
       );
     }
-    splitString(allStrings, regEx);
-    //trying to find non-unique items
-    uniqueItems = arr.filter((item, index) => {
-      return arr.indexOf(item) !== index;
-    });
+    splitString(allTextAreaValue, regEx);
+
+    console.log(arr);
     console.log(`Repeated items: ${uniqueItems}`);
-    document.getElementById('Duplicates').innerHTML = uniqueItems;
+    document.getElementById('Duplicates').innerHTML = '<span id="duplicatesItems" style="font-size: 24px; color: black">' + uniqueItems;
+  }
+});
+
+searchDuplicates.addEventListener('input', () => {
+  const opar = document.getElementById('titleText').value; //foind text in textarea
+
+  function highlight() {
+    let inputText = document.getElementById('amountArea'); //inputText from Title textarea
+    let search = document.getElementById('duplicatesItems').innerHTML; // text from title input textarea
+
+    search = search.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'); //https://stackoverflow.com/questions/3446170/escape-string-for-use-in-javascript-regex
+
+    let re = new RegExp(search, 'g');
+    let m;
+    if (search.length > 0) {
+      inputText.innerHTML = opar.replace(re, `<mark>$&</mark>`);
+    } else {
+      inputText.innerHTML = opar;
+    }
   }
 });
 
