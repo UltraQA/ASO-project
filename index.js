b414819304cac71ea6a6ad24ed18bebf38de5a70
Andrei <andrei_virys@mail.ru>
2023-05-31 12:10:23 +0200

    - added NavBar and DeepLink logic to the page

--- index.js
+++ index.js
@@ -1,21 +1,10 @@
-//TODO :
-// 1. I have a button
-// 2. After clicking on button I want to see duplicates words
-// 3. Sort duplicates by ABC
-
-
-/**
- * @Взять весь текст из поля @Duplicate_Items
- * @Перевести весь текст в JSON и разбить на массив через ','
- * @Брать аждый элемент массива и сравнивать с текстом в Полях: Title, SubTitle, KeyWords
- * @Подсвечивать слово в полях, если оно совпало со словом, которе есть в строке Duplicate Items
- */
-
 import {TitleArea} from './src/js/title.js';
 import {SubTitle} from './src/js/subTitle.js';
 import {KeyWords} from './src/js/keyWords.js';
 import {DuplicatesArea} from './src/js/duplicatesArea.js';
 import {Logic} from './src/js/logic.js';
+import {NavBar} from "./src/js/navBar.js";
+import {DeepLinks} from "./src/js/deepLinks.js"
 
 TitleArea.addWordsAmount();
 SubTitle.addWordsAmount();
@@ -26,32 +15,48 @@ Logic.setMaxLengthForTextArea(SubTitle.subTitle, 30, SubTitle.subTitleCharacters
 Logic.setMaxLengthForTextArea(KeyWords.keyWords, 100, KeyWords.keyWordsCharacters);
 
 KeyWords.showDuplicatesButton.addEventListener('click', () => {
-	DuplicatesArea.cleanDuplicatesArea(DuplicatesArea.duplicatesArea);
-	Logic.showDuplicatesForAllAreas(
-		TitleArea.titleElement,
-		SubTitle.subTitle,
-		KeyWords.keyWords
-	);
+    DuplicatesArea.cleanDuplicatesArea(DuplicatesArea.duplicatesArea);
+    Logic.showDuplicatesForAllAreas(
+        TitleArea.titleElement,
+        SubTitle.subTitle,
+        KeyWords.keyWords
+    );
 });
 
 DuplicatesArea.cleanAllTextAreasButton.addEventListener('click', () => {
-	DuplicatesArea.cleanTextArea(TitleArea.titleElement);
-	DuplicatesArea.cleanTextArea(SubTitle.subTitle);
-	DuplicatesArea.cleanTextArea(KeyWords.keyWords);
+    DuplicatesArea.cleanTextArea(TitleArea.titleElement);
+    DuplicatesArea.cleanTextArea(SubTitle.subTitle);
+    DuplicatesArea.cleanTextArea(KeyWords.keyWords);
 
-	TitleArea.cleanCharacterArea(TitleArea.titleCharacters);
-	TitleArea.cleanTitleWordsAmount(TitleArea.titleWordsAmount);
+    TitleArea.cleanCharacterArea(TitleArea.titleCharacters);
+    TitleArea.cleanTitleWordsAmount(TitleArea.titleWordsAmount);
 
-	SubTitle.cleanCharacterArea(SubTitle.subTitleCharacters);
-	SubTitle.cleanSubTitleWordsAmount(SubTitle.subTitleWordsAmount);
+    SubTitle.cleanCharacterArea(SubTitle.subTitleCharacters);
+    SubTitle.cleanSubTitleWordsAmount(SubTitle.subTitleWordsAmount);
 
-	KeyWords.cleanCharacterArea(KeyWords.keyWordsCharacters);
-	KeyWords.cleanKeyWordsAmount(KeyWords.keyWordsAmount);
+    KeyWords.cleanCharacterArea(KeyWords.keyWordsCharacters);
+    KeyWords.cleanKeyWordsAmount(KeyWords.keyWordsAmount);
 
-	DuplicatesArea.cleanDuplicatesArea(DuplicatesArea.duplicatesArea);
+    DuplicatesArea.cleanDuplicatesArea(DuplicatesArea.duplicatesArea);
 });
 
+NavBar.googlePlay.addEventListener('click', () => {
+    NavBar.changeVisibilityToGooglePlay();
+})
+NavBar.appStore.addEventListener('click', () => {
+    NavBar.changeVisibilityToAppStore();
+})
+
+NavBar.deepLinks.addEventListener('click', () => {
+    NavBar.changeVisibilityToDeepLinks();
+})
+
+DeepLinks.generateLinkBtn.addEventListener('click', () => {
+    console.log(DeepLinks.getRawLink());
+    DeepLinks.generateYouTubeDeepLink(DeepLinks.getRawLink());
+})
 
 
-
-
+// Usage example
+// const youtubeUrl = "https://www.youtube.com/watch?v=abc123";
+// generateYouTubeDeepLink(youtubeUrl);
