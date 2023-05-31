083cb38fb26f59660d24b193b40d082a28e310f0
Andrei <andrei_virys@mail.ru>
2023-05-31 12:09:41 +0200

    - added main Area class

---
+++ src/js/mainArea.js
@@ -0,0 +1,9 @@
+export class MainArea {
+    static mainAreaAppStore = document.querySelector('#mainAreaAppStore');
+    static mainAreaGooglePlay = document.querySelector('#mainAreaGooglePlay');
+    static deepLinkArea = document.querySelector('#deepLinkArea');
+
+    constructor() {
+    }
+}
+
