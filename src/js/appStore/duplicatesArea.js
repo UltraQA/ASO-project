9f0d0d7f1a808a9063702d150a2d7d0817d84b3c
Andrei <andrei_virys@mail.ru>
2023-06-05 09:45:08 +0200

    - added new html elements into DuplicatesArea

--- src/js/appStore/duplicatesArea.js
+++ src/js/appStore/duplicatesArea.js
@@ -2,6 +2,9 @@ export class DuplicatesArea {
 
     static cleanAllTextAreasButton = document.querySelector('#cleanAllTextAreasButton');
     static duplicatesArea = document.querySelector('#amountDuplicatesItems');
+    static duplicatesAreaGP = document.querySelector('#amountDuplicatesItemsGP');
+    static showDuplicatesButton = document.querySelector('#showDuplicatesButton');
+    static showDuplicatesButtonGP = document.querySelector('#showDuplicatesButtonGP');
 
     constructor() {
     }
